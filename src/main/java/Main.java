026e6e5ace223c70362da85dc3625f8ead0f91df
vladyour <vlad.your8@gmail.com>
2017-06-08 14:12:52 +0300

    Final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,50 @@
-//import org.hibernate.HibernateException;
-//import org.hibernate.Metamodel;
-//import org.hibernate.query.Query;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.cfg.Configuration;
-//
-//import javax.persistence.metamodel.EntityType;
-//
-//import java.util.Map;
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
 
 /**
  * Created by vladyour on 06.06.17.
  */
 public class Main {
-//    private static final SessionFactory ourSessionFactory;
-//
-//    static {
-//        try {
-//            Configuration configuration = new Configuration();
-//            configuration.configure();
-//
-//            ourSessionFactory = configuration.buildSessionFactory();
-//        } catch (Throwable ex) {
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static Session getSession() throws HibernateException {
-//        return ourSessionFactory.openSession();
-//    }
-//
-//    public static void main(final String[] args) throws Exception {
-//        final Session session = getSession();
-//        try {
-//            System.out.println("querying all the managed entities...");
-//            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
-//            for (EntityType<?> entityType : metamodel.getEntities()) {
-//                final String entityName = entityType.getName();
-//                final Query query = session.createQuery("from " + entityName);
-//                System.out.println("executing: " + query.getQueryString());
-//                for (Object o : query.list()) {
-//                    System.out.println("  " + o);
-//                }
-//            }
-//        } finally {
-//            session.close();
-//        }
-//    }
-public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-}
+    private static final SessionFactory ourSessionFactory;
 
-    @RequestMapping("/")
-    String index() {
-        return "index";
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        try {
+            System.out.println("querying all the managed entities...");
+            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                final String entityName = entityType.getName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
+        }
     }
 }
